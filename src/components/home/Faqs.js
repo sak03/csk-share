78aebb92218c5631b15fdabaa77bf4597e1735dd
sak03 <sartaj2394@gmail.com>
2025-02-12 03:47:45 +0530

    updated

--- src/components/home/Faqs.js
+++ src/components/home/Faqs.js
@@ -5,29 +5,29 @@ import { FiMinusCircle, FiPlusCircle } from "react-icons/fi";
 
 const Faqs = () => {
     const [selectedFaq, setSelectedFaq] = useState(null)
-  return (
-    <div>
+    return (
         <div>
-        <h1 className='text-3xl'>FAQ's</h1>
-          </div>
-          <div>
-              {faqDatas?.map((item)=>{
-                return(
-                    <div key={item.id}>
-                        <div className='flex justify-between pointer' onClick={() => setSelectedFaq(item)}>
-                            <strong>{item.question}</strong>
-                            <span>{selectedFaq?.id === item?.id ? <FiMinusCircle /> : <FiPlusCircle/>}</span>
+            <div className='flex justify-center'>
+                <h1 className='text-3xl'>FAQ's</h1>
+            </div>
+            <div>
+                {faqDatas?.map((item) => {
+                    return (
+                        <div key={item.id}>
+                            <div className='flex justify-between pointer' onClick={() => setSelectedFaq(item)}>
+                                <strong>{item.question}</strong>
+                                <span>{selectedFaq?.id === item?.id ? <FiMinusCircle /> : <FiPlusCircle />}</span>
+                            </div>
+                            {selectedFaq?.id === item?.id ? <div>
+                                <small>{item.answer}</small>
+                            </div> : ""}
+                            {item?.isLast ? "" : <div className='footer-line'></div>}
                         </div>
-                        {selectedFaq?.id === item?.id ?  <div>
-                            <small>{item.answer}</small>
-                        </div> : ""}
-                        {item?.isLast  ? "" : <div className='footer-line'></div>}
-                    </div>
-                )
-              })}
-          </div>
-    </div>
-  )
+                    )
+                })}
+            </div>
+        </div>
+    )
 }
 
 export default Faqs
