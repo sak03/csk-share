438fa152d866600374044cf1f2da17fe0fe37cf6
sak03 <sartaj2394@gmail.com>
2025-02-12 14:00:18 +0530

    updated

--- src/components/home/CskShareTable.js
+++ src/components/home/CskShareTable.js
@@ -1,27 +1,54 @@
 "use client"
 import React from 'react';
 import { fundamentalDatas } from "@/utils/datas/fundamentalsData"
+import { shareholdingPatternsDatas } from '@/utils/datas/shareholdingPatternsData'
 
 const CskShareTable = () => {
-  return (
-      <div className='share-table-body'>
-          <h4 className='text-2xl font-bold my-3'>Fundamentals</h4>
-          <div className="w-[75%]">
-          <div className='grid grid-cols-4 gap-4 text-left'>
-              {fundamentalDatas?.map((item)=>{
-                return(
-                    <div className='contents' key={item.id}>
-                        <span className='py-1 font-medium'>{item.title}</span>
-                        <span className='py-1 font-medium'>{item.initialAmt}</span>
-                        <span className='py-1 font-medium'>{item.shareType}</span>
-                        <span className='py-1 font-medium'>{item.finalAmt}</span>
-                    </div>
-                )
-              })}
-          </div>
-          </div>
-      </div>
-  )
+    return (
+        <div className='share-table-body'>
+            <div className="w-[75%]">
+                <h4 className='text-2xl font-bold my-3'>Fundamentals</h4>
+                <div className='grid grid-cols-4 gap-4 text-left'>
+                    {fundamentalDatas?.map((item) => {
+                        return (
+                            <div className='contents' key={item.id}>
+                                <span className='py-1 font-medium'>{item.title}</span>
+                                <span className='py-1 font-medium'>{item.initialAmt}</span>
+                                <span className='py-1 font-medium'>{item.shareType}</span>
+                                <span className='py-1 font-medium'>{item.finalAmt}</span>
+                            </div>
+                        )
+                    })}
+                </div>
+                <h4 className='text-2xl font-bold my-5'>Shareholding Pattern</h4>
+                <div className='border border-gray-300 p-3 rounded-xl'>
+                    <table className="w-full border-collapse border border-gray-300 rounded-lg overflow-hidden">
+                        <thead className="text-gray-400">
+                            <tr>
+                                <th className="border-y border-gray-300 px-4 py-3 text-left">Shareholding Pattern</th>
+                                <th className="border-y border-gray-300 px-4 py-3 text-left">2021</th>
+                                <th className="border-y border-gray-300 px-4 py-3 text-left">2022</th>
+                                <th className="border-y border-gray-300 px-4 py-3 text-left">2023</th>
+                                <th className="border-y border-gray-300 px-4 py-3 text-left">2024</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {shareholdingPatternsDatas?.map((item) => (
+                                <tr key={item.id} className="hover:bg-gray-50">
+                                    <td className="border-y border-gray-300 px-4 py-3">{item.holdingPattern}</td>
+                                    <td className="border-y border-gray-300 px-4 py-3">{item.year1}</td>
+                                    <td className="border-y border-gray-300 px-4 py-3">{item.year2}</td>
+                                    <td className="border-y border-gray-300 px-4 py-3">{item.year3}</td>
+                                    <td className="border-y border-gray-300 px-4 py-3">{item.year4}</td>
+                                </tr>
+                            ))}
+                        </tbody>
+                    </table>
+                </div>
+
+            </div>
+        </div>
+    )
 }
 
 export default CskShareTable
---
+++ src/utils/datas/shareholdingPatternsData.js
@@ -0,0 +1,43 @@
+export const shareholdingPatternsDatas = [
+    {
+        id:"shp01",
+        holdingPattern:"Trustees, India Cements Shareholders Trust",
+        year1:"30.06%",
+        year2:"30.03%",
+        year3:"21.47%",
+        year4:"36.22%"
+    },
+    {
+        id: "shp02",
+        holdingPattern: "Sri Saradha Logistics Private Limited",
+        year1: "6.89%",
+        year2: "6.89%",
+        year3: "6.89%",
+        year4: "5.26%"
+    },
+    {
+        id: "shp03",
+        holdingPattern: "Life Insurance Corporation Of India",
+        year1: "6.04%",
+        year2: "6.04%",
+        year3: "6.04%",
+        year4: "5.60%"
+    },
+    {
+        id: "shp04",
+        holdingPattern: "Trustees",
+        year1: "-",
+        year2: "-",
+        year3: "1.76%",
+        year4: "4.90%"
+    },
+    {
+        id: "shp05",
+        holdingPattern: "Others",
+        year1: "57.01%",
+        year2: "57.04%",
+        year3: "57.36%",
+        year4: "48.02%"
+    },
+   
+]
