8c970fd69a109b2dc17e5233b55a2075df300e51
sak03 <sartaj2394@gmail.com>
2025-02-13 11:21:58 +0530

    updated

--- src/components/home/Landing.js
+++ src/components/home/Landing.js
@@ -3,7 +3,7 @@ const Landing = () => {
     return (
         <div className='landing-section'>
             <div className='landing-section2'>
-                <h1>Chennai Super King (CSK) Shares</h1>
+                <h1>Chennai Super Kings (CSK) Shares</h1>
             </div>
         </div>
     )
